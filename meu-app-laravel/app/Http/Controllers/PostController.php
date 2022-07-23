4390e478da085e89d80cc26862defbc984831eee
Danilo Nogueira <nvdanilo@gmail.com>
2022-07-22 21:41:52 -0300

    Relacionamento Pt 1

---
+++ meu-app-laravel/app/Http/Controllers/PostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+use PDO;
+
+class PostController extends Controller
+{
+    protected $user;
+    protected $post;
+
+    public function __construct(User $user, Post $post)
+    {
+        $this->user = $user;
+        $this->post = $post;
+    }
+
+    public function index()
+    {
+        // if (!$user = $this->user->find($userId)){
+        //     return redirect()->back();
+        // }
+
+        $posts = $this->post->all();
+
+        return view("posts.index", compact("posts"));
+    }
+}
---
+++ meu-app-laravel/resources/views/posts/index.blade.php
@@ -0,0 +1,29 @@
+@extends("template.users")
+@section("title", "Listagem de Postagens")
+@section("body")
+
+    <h1 class="text-center">Listagem de Postagens</h1>
+
+
+    <table class="table table-dark table-hover table-bordered table-striped">
+        <thead class="text-center">
+            <tr>
+                <th scope="col">#ID</th>
+                <th scope="col">Título</th>
+                <th scope="col">Postagem</th>
+                <th scope="col">Data de Cadastro</th>
+            </tr>
+        </thead>
+        <tbody class="text-center">
+            @foreach($posts as $post)
+                <tr>
+                    <th scope="row">{{ $post->id }}</th>
+                    <td>{{ $post->title }}</td>
+                    <td>{{ $post->post }}</td>
+                    <td>{{ date("H:m | d/m/Y", strtotime($post->created_at)) }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+@endsection
